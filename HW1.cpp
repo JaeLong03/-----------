3500284308f21bd57dc7356d690e195787333310
JaeLong03 <jaehwan4562@gmail.com>
2025-03-20 23:47:35 +0900

    DS 3.20

---
+++ HW1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+class weight{ 
+    int kg, gram; 
+
+    public: 
+    void set_weight(int n1, int n2); 
+    int get_weight(); 
+}; 
+
+void weight:: set_weight(int n1, int n2){
+    kg = n1; 
+    gram = n2; 
+}
+
+int weight:: get_weight(){ 
+    int result; 
+    
+    result = kg * 1000 + gram; 
+    return result; 
+}
+
+weight add_weight(weight w1, weight w2); 
+bool less_than(weight w1, weight w2); 
+
+int main(){ 
+    weight w1, w2, w3; 
+    w1.set_weight(12, 800); 
+    w2.set_weight(2, 900);
+    w3 = add_weight(w1, w2);
+    
+    cout << w3.get_weight() << " grams\n"; 
+
+    if(less_than(w1,w2))
+        cout << "yes\n"; 
+    else
+        cout << "no\n"; 
+}
+
+weight add_weight(weight w1, weight w2){ 
+    weight w3; 
+
+    int sum = w1.get_weight() + w2.get_weight(); 
+    w3.set_weight(sum/1000, sum%1000); 
+    return w3; 
+}
+
+bool less_than(weight w1, weight w2){ 
+    if(w1.get_weight() < w2.get_weight()){ 
+        return true; 
+    }
+    else
+        return false; 
+}
