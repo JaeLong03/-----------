8df6ae5a956df4a12414bb53cdaa49a08fd2e7d4
JaeLong03 <jaehwan4562@gmail.com>
2025-03-30 11:52:37 +0900

    palindrome 3.30

--- HW3.cpp
+++ HW3.cpp
@@ -21,14 +21,14 @@ op_stack:: op_stack(){
     top = 0;
 }
 
-void op_stack:: push(char x){ 
-    s[top] = x; 
+void op_stack::push(char x) { 
+    s[top] = x;  
     top++; 
 }
 
-char op_stack:: pop(){ 
-    top--; 
-    return(s[top]); 
+char op_stack::pop() { 
+    top--;  
+    return s[top];  
 }
 
 bool op_stack:: empty(){ 
@@ -39,13 +39,43 @@ char op_stack:: top_element(){
     return(s[top-1]); 
 }
 
-int main(){ 
+bool is_Palin(string s); 
 
+int main() { 
     string str; 
-    int size; 
 
     cin >> str; 
-    size = str.size() / 2; 
+
+    if (is_Palin(str)) { 
+        cout << "Yes, it's a palindrome" << endl; 
+    } else { 
+        cout << "No, it's not a palindrome" << endl; 
+    }
 
     return 0; 
+}
+
+bool is_Palin(string s) { 
+    op_stack st;
+    op_stack(); 
+    int len = s.size();
+    
+    for (int i = 0; i < len / 2; i++) { 
+        st.push(s[i]); 
+    }
+
+    int start_idx;
+
+    if(len % 2 == 0) 
+        start_idx = len / 2;
+    else   
+        start_idx = len / 2 + 1;
+
+    for (int i = start_idx; i < len; i++) { 
+        if (s[i] != st.pop()) { 
+            return false; 
+        }
+    }
+    
+    return true; 
 }
